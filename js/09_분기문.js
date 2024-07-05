c89853ef7d7a7dbb9a21eb667e76c9d348d699b6
LeeNS-2024 <dlsktj1@gmail.com>
2024-07-05 17:28:17 +0900

    no message

--- js/09_분기문.js
+++ js/09_분기문.js
@@ -76,26 +76,26 @@ function check4() {
 function startGame() {
 
   // 맞춰야되는 난수(1 ~ 200) 발생
-  const answer = Math.floor(Math.random() * 200) +1;
+  const answer = Math.floor(Math.random() * 200) + 1;
 
   // 테스트 후 삭제
   // console.log("정답 :", answer);
 
   //--------------------------------------------------------------------
-  
+
   // 정답 시도 횟수를 세기 위한 변수 선언
   let count = 0;
-  
+
   //--------------------------------------------------------------------
-  
+
   let str; // promp에 출력할 문자열 
 
-  while(true){ // 무한 반복
-    
+  while (true) { // 무한 반복
+
     // 숫자만 정상 입력한다고 가정
     let input = prompt(str);
 
-    if(input === null){ // 취소 클릭 시
+    if (input === null) { // 취소 클릭 시
       alert("게임 포기");
       break;
     }
@@ -107,18 +107,18 @@ function startGame() {
 
     // nan (not a number : 숫자가 아니다)
     // isNaN(값) : 값이 nan이면 true
-    if( isNaN(value)){ // 숫자가 아닌 값을 입력한 경우
+    if (isNaN(value)) { // 숫자가 아닌 값을 입력한 경우
       alert("숫자만 입력해 주세요")
       continue;
     }
 
     /* 범위(1 ~ 200)에 초과하는 값을 입력했을 시 */
-    if(value < 1 || value > 200){ // 범위 초과
+    if (value < 1 || value > 200) { // 범위 초과
       alert("1 ~ 200 사이 값만 작성해 주세요");
       continue;
     }
 
-    
+
 
 
 
@@ -127,12 +127,12 @@ function startGame() {
     count++;
 
     /* 정답인 경우 */
-    if(value === answer){
+    if (value === answer) {
       alert(`정답 !!!(${answer})/ 시도 횟수 : ${count}`);
       break;
     }
     /* 정답이 아닌 경우 */
-    if(value < answer){ // 작은 경우
+    if (value < answer) { // 작은 경우
       str = `${value} [UP] / 시도 횟수 : ${count}회`;
     } else { // 큰 경우
       str = `${value} [DOWN] / 시도 횟수 : ${count}회`;
